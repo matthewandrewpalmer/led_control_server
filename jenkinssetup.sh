caba3601f71b1b687b9dc2b700161399a6890bf6
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2018-04-15 20:34:09 +0100

    Create jenkinssetup.sh

---
+++ jenkinssetup.sh
@@ -0,0 +1,10 @@
+# ====================== Installing Jenkins ==================================
+echo "====================== Installing Jenkins =================================="
+
+cd ~ 
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins -y
+sudo systemctl start jenkins.service
+sudo systemctl status jenkins.service
