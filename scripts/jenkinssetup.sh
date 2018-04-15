c49d1b849ee9d8010a3d8b0a1f69e7922852d424
Matthew Palmer <matthewandrewpalmer@outlook.com>
2018-04-15 22:34:02 +0100

    edited script

--- scripts/jenkinssetup.sh
+++ scripts/jenkinssetup.sh
@@ -7,3 +7,6 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
+
+sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp
+sudo firewall-cmd --reload
