cb6fb5c8c120074aab5b3ef396c218c9ec9b9ca1
Matthew Palmer <matthewandrewpalmer@outlook.com>
2018-04-16 21:25:54 +0100

    removed java script

--- scripts/java_change.sh
+++
@@ -1,7 +0,0 @@
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt update
-sudo apt install oracle-java8-installer
-
-
-update-java-alternatives -l
-#sudo update-java-alternatives -s java-8-oracle
